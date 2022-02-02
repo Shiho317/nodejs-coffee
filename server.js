6222448b36d898d5561a6263bce50f91e2003be6
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-01 18:16:55 -0800

    setted up environment

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 const path = require("path");
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, ''));
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/styles', express.static('styles'))
+
+app.get("/", (req, res) => {
+  res.render('index', {});
+})
 
 app.listen(3000, () => {
   console.log('Running')
