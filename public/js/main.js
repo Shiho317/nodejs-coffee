80bdbca8a49c7fec2770ec1ac64d82a8ce8706f2
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-03 15:16:27 -0800

    added scroll function

--- public/js/main.js
+++ public/js/main.js
@@ -11,37 +11,92 @@ linksArr.addEventListener('click', e => {
   })
 });
 
-const swiper = new Swiper('.swiper', {
-    direction: 'vertical',
-    sliderPerView: 5,
-    spaceBetween: 0,
-    mousewheel: {
-          invert: false,
-        },
-    // pagination: {
-    //     el:'.swiper-pagination',
-    // }
-})
+console.log(targetElement.getBoundingClientRect().top);
 
-// const swiper = new Swiper('.swiper', {
-//   direction: 'vertical',
-//   mousewheel: {
-//     invert: true,
-//   },
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+// const mouseWheelScroll = () => {
+
+//   const sliders = document.querySelectorAll('.swiper-slide');
+//   console.log(sliders);
+
+//   sliders.forEach(slider => {
+//   const targetTop = slider.getBoundingClientRect().top;
+//   console.log(targetTop)
+
+//   for(let i = 0; i < sliders.length; i++){
+//     const mouseTop = sliders[i].getBoundingClientRect().top;
+//     console.log(mouseTop)
+//     scrollTop = sliders[i+1].getBoundingClientRect().top;
+//   }
+
+//   window.scrollTo({
+//     top: scrollTop,
+//     behavior: 'smooth'
+//   })
+// }
+// }
+
+  let topArr = [];
+
+  const sliders = document.querySelectorAll('.swiper-slide');
+  // console.log(sliders);
+
+  const sliderWrap = document.querySelector('.swiper-wrapper');
+
+  
+
+    sliders.forEach((slider, index, array) => {
+      slider.addEventListener('wheel', (e) => {
+        e.preventDefault();
+
+        console.log(index)
+
+        console.log(index + 1)
+
+        const scrollTop = window.pageYOffset + array[index + 1].getBoundingClientRect().top;
+
+        console.log(scrollTop);
+
+      window.scrollTo({
+        top: scrollTop,
+        behavior: "smooth"
+      })
+      } )
+    })
+
+
+    
+
+//     sliders.forEach(slider => {
+//   const targetTop = slider.getBoundingClientRect().top;
+
+  
+
+//   console.log(targetTop)
+  
+
+  
+  
 // });
 
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
+// const mouseWheelScroll = () => {
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-// });
+
+// for(let i = 0; i < sliders.length; i++){
+//     const mouseTop = sliders[i].getBoundingClientRect().top;
+//     console.log(mouseTop)
+//     topArr.push(mouseTop);
+//     console.log(topArr);
+
+//     switch(window.pageYOffset){
+//       case mouseTop[0]:
+//         window.scrollTo({
+//               top: mouseTop[1],
+//               behavior: 'smooth'
+//             })
+//     }
+//   }
+
+//   }
+
+//   window.addEventListener('wheel', mouseWheelScroll)
